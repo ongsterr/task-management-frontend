b04830ab5c5c44f97a2c65ca314b28ba7835f065
ongsterr <ongsterr@users.noreply.github.com>
2021-09-12 12:37:43 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.1a1838a1ee3263610d4568a1ae0cff96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5184f3cd939b9f2280e0a2251e1afe50",
+    "url": "https://ongsterr.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "3b8d4c43e4443c5fd113",
+    "url": "https://ongsterr.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "53dd68423903ee23f72e",
+    "url": "https://ongsterr.github.io/task-management-frontend/static/js/2.298a3cdb.chunk.js"
+  },
+  {
+    "revision": "3b8d4c43e4443c5fd113",
+    "url": "https://ongsterr.github.io/task-management-frontend/static/js/main.bbd56e04.chunk.js"
+  },
+  {
+    "revision": "42dfa4c55079324dd553",
+    "url": "https://ongsterr.github.io/task-management-frontend/static/js/runtime~main.c81082a9.js"
+  }
+]);
